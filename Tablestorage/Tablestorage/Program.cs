af5778a0833cefa3e396d341c9a722c7c0309672
amrutamusale <musaleamruta1@gmail.com>
2022-07-07 17:04:57 +0530

    initial commit

---
+++ Tablestorage/Tablestorage/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+
+namespace Tablestorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var client = new TableClient(new Uri("https://levelupsolution0016.table.core.windows.net"), "tblSample", new TableSharedKeyCredential("levelupsolution0016", "g2R5jhXdl4zcgPKi3qABwZJLPhyOqo+FAdgKiltiU9WtcA0aYQNtbb67EflGV0ZswEbZKCzxIGCt+AStIYRE3w=="));
+            Pageable<TableEntity> queryableTable = client.Query<TableEntity>();
+            foreach (TableEntity item in queryableTable)
+            {
+                Console.WriteLine(item.GetString("Name"));
+            }
+        }
+    }
+}
+        
