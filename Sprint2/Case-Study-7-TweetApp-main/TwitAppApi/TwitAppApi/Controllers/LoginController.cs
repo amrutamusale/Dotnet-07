19fbdffb2eb610566c62c6eeccbba28a4caf41aa
amrutamusale <musaleamruta1@gmail.com>
2022-07-24 14:50:42 +0530

    initial commit

---
+++ Sprint2/Case-Study-7-TweetApp-main/TwitAppApi/TwitAppApi/Controllers/LoginController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TwitAppApi.Interfaces;
+using TwitAppApi.Models;
+using TwitAppApi.ViewModels;
+using Microsoft.AspNetCore.Http;
+
+namespace TwitAppApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        TweetDBContext db;
+        IJWTManagerRepository iJWTMangerRepository;
+
+        //public object WebSecurity { get; private set; }
+        //public object EmailManager { get; private set; }
+
+        public LoginController(TweetDBContext _db, IJWTManagerRepository _iJWTMangerRepository)
+        {
+            db = _db;
+            iJWTMangerRepository = _iJWTMangerRepository;
+        }
+
+        [HttpGet]
+        [Route("User")]
+        public IEnumerable<TblLogin> Get()
+        {
+            return db.TblLogins;
+        }
+
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            var token = iJWTMangerRepository.Authenicate(loginViewModel, false);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(token);
+        }
+
+
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            LoginViewModel login = new LoginViewModel();
+            login.FirstName = registerViewModel.FirstName;
+            login.LastName = registerViewModel.LastName;
+            login.Email = registerViewModel.Email;
+            login.LoginId = registerViewModel.LoginId;
+            login.Password = registerViewModel.Password;
+            login.ConfirmPassword = registerViewModel.ConfirmPassword;
+            login.ContactNumber =  registerViewModel.ContactNumber;
+            var token = iJWTMangerRepository.Authenicate(login, true);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(token);
+        }
+
+        /*[HttpGet("forgot-password")]
+          public ActionResult ForgotPassword()
+          {
+              return View();
+          }
+         [HttpPost("forgot-password")]
+          public ActionResult ForgotPassword(ForgotPasswordModel model)
+          {
+              return View();
+          }
+              if (ModelState.IsValid)
+              {
+
+              return View();
+          }*/
+    }
+}
---
+++ Sprint2/Case-Study-7-TweetApp-main/TwitAppApi/TwitAppApi/Models/TweetDBContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace TwitAppApi.Models
+{
+    public partial class TweetDBContext : DbContext
+    {
+        public TweetDBContext()
+        {
+        }
+
+        public TweetDBContext(DbContextOptions<TweetDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblLogin> TblLogins { get; set; }
+        public virtual DbSet<TblTweet> TblTweets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Data Source=Amruta;Initial Catalog=TweetDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TblLogin>(entity =>
+            {
+                entity.ToTable("TblLogin");
+
+                entity.Property(e => e.ConfirmPassword).HasMaxLength(100);
+
+                entity.Property(e => e.Email).HasMaxLength(100);
+
+                entity.Property(e => e.FirstName).HasMaxLength(100);
+
+                entity.Property(e => e.LastName).HasMaxLength(100);
+
+                entity.Property(e => e.LoginId).HasMaxLength(100);
+
+                entity.Property(e => e.Password).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<TblTweet>(entity =>
+            {
+                entity.ToTable("TblTweet");
+
+                entity.Property(e => e.AuthorEmail).HasMaxLength(100);
+
+                entity.Property(e => e.AuthorLogo).HasMaxLength(100);
+
+                entity.Property(e => e.AuthorSlug).HasMaxLength(100);
+
+                entity.Property(e => e.TweetDescription).HasMaxLength(1000);
+
+                entity.Property(e => e.TweetImage).HasMaxLength(100);
+
+                entity.Property(e => e.TweetTime).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
